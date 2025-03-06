ba92fa0d4bd39bd747914772bcaf7c5b9e054a27
PrinceJoshwa <sprincejoshwa85@gmail.com>
2025-03-07 00:08:34 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
+import CreateEvent from "./pages/CreateEvent";
+import BookingDetails from "./pages/BookingDetails";
+import EditBooking from "./pages/EditBooking"
+import BookingPage from "./pages/BookingPage";
+import UserDetailsForm from './pages/UserDetailsForm';
+import BookingConfirmed from './pages/BookingConfirmed';
+import Scheduled from "./pages/Scheduled";
 
 const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || '695627827164-7e7vci68epagob98akuovcjcfljsc72i.apps.googleusercontent.com';
 
@@ -12,6 +19,14 @@ function App() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/scheduled" element={<Scheduled/>} />
+          <Route path="/create-event/:eventType" element={<CreateEvent />} />
+          <Route path="/booking-details/:bookingId" element={<BookingDetails />} />
+          <Route path="/edit-booking/:bookingId" element={<EditBooking />} />
+          <Route path="/:username/:eventTitle" element={<BookingPage />} />
+          <Route path="/user-details" element={<UserDetailsForm />} />
+          <Route path="/booking-confirmed" element={<BookingConfirmed />} />
       </Routes>
     </Router>
    </GoogleOAuthProvider>
