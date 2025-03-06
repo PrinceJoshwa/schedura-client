74ef53e985e5f0a87900a10428d0b3e57ee01514
PrinceJoshwa <sprincejoshwa85@gmail.com>
2025-03-06 21:17:27 +0530

    first commit

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,4 @@
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_API_URL || 'https://schedura-server-page.vercel.app/api';
 
 export const saveResume = async (resumeData, token) => {
   try {
